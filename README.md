bcb768a2e873fe2a8fcf3892bc137679837a00ca
rodrigomoya <rodrigo@rizoma.io>
2016-08-24 15:48:17 -0300

    fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# GroveI2CTempHumiSht31
+# grove_i2c_temp_humi_hdc1000
+grove_i2c_temp_humi_hdc1000
---
+++ grove_i2c_temp_humi_sht31.cpp
@@ -0,0 +1,54 @@
+/*
+ * grove_i2c_temp_humi_sht31.cpp
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Lambor, Jack
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "suli2.h"
+#include "grove_i2c_temp_humi_sht31.h" 
+
+
+/*comment*/
+
+GroveI2CTempHumiSht31::GroveI2CTempHumiSht31(int pinsda, int pinscl)
+{
+    this->i2c = (I2C_T *)malloc(sizeof(I2C_T));
+    suli_i2c_init(i2c, pinsda, pinscl);
+
+    Wire.begin();
+}
+
+bool GroveI2CTempHumiSht31::read_temperature(float *temperature)
+{
+    *temperature = 1
+    return true;
+}
+
+bool GroveI2CTempHumiSht31::read_humidity(float *humidity)
+{
+
+    *humidity = 1
+    return true;
+}
